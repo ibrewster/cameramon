f99b8d091c2f7a42d2c8f8b5a6ae5bd24221187a
Israel Brewster <israel@brewstersoft.net>
2022-10-21 10:22:16 -0800

    Remove some older, unused files

--- zmdetectd.py
+++
@@ -1,6 +0,0 @@
-from zmdetectd import app
-import bjoern
-
-if __name__ == "__main__":
-    # app.run(host = '0.0.0.0', port = 5002, threaded = False, processes = 1)
-    bjoern.run(app, host = '0.0.0.0', port = 5003)
