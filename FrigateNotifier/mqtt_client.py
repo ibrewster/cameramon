dab66b3d1fa664e7e6f6bf7a70ddf3096111d6c6
Israel Brewster <ijbrewster@alaska.edu>
2026-01-22 15:26:35 -0900

    Make payload say what type of delivery was detected

--- FrigateNotifier/mqtt_client.py
+++ FrigateNotifier/mqtt_client.py
@@ -87,7 +87,8 @@ def on_message(client, userdata, msg):
 
         if item_type == 'package' or delivery_vehicle:
             logging.info("!!!PACKAGE DELIVERY!!!")
-            notify.send_custom('detected', 'cameramon/delivery')
+            payload = 'usps' if delivery_vehicle == 'usps' else 'parcel'
+            notify.send_custom(payload, 'cameramon/delivery')
 
         if obj.is_moving:
             # Save the payload
